a9283c1e131e6a2fee2d5d58839a0adb9a2dc8ae
Robert Raj <angularrobert@gmail.com>
2020-11-05 15:55:03 +0530

    Update ng-table-filtering.directive.ts
    Angular10 Support

--- components/table/ng-table-filtering.directive.ts
+++ components/table/ng-table-filtering.directive.ts
@@ -1,9 +1,5 @@
-import { Directive, EventEmitter, ElementRef, Renderer, HostListener, Input, Output } from '@angular/core';
+import { Directive, EventEmitter, ElementRef, Renderer2, HostListener, Input, Output } from '@angular/core';
 
-// import {setProperty} from 'angular2/ts/src/core/forms/directives/shared';
-function setProperty(renderer:Renderer, elementRef:ElementRef, propName:string, propValue:any):void {
-  renderer.setElementProperty(elementRef, propName, propValue);
-}
 
 @Directive({selector: '[ngTableFiltering]'})
 export class NgTableFilteringDirective {
@@ -24,7 +20,7 @@ export class NgTableFilteringDirective {
   }
 
   private element:ElementRef;
-  private renderer:Renderer;
+  private renderer:Renderer2;
 
   @HostListener('input', ['$event.target.value'])
   public onChangeFilter(event:any):void {
@@ -32,10 +28,11 @@ export class NgTableFilteringDirective {
     this.tableChanged.emit({filtering: this.ngTableFiltering});
   }
 
-  public constructor(element:ElementRef, renderer:Renderer) {
+  public constructor(element:ElementRef, renderer:Renderer2) {
     this.element = element;
     this.renderer = renderer;
     // Set default value for filter
-    setProperty(this.renderer, this.element, 'value', this.ngTableFiltering.filterString);
+    //setProperty(this.renderer, this.element, 'value', this.ngTableFiltering.filterString);
+    this.renderer.setProperty(this.element.nativeElement, 'value', this.ngTableFiltering.filterString);
   }
 }
